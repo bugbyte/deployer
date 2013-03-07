b83d6fcc386ff8122b14a5b27e72357feae11311
Bert-Jan de Lange <bert-jan@bugbyte.nl>
2013-03-07 13:03:09 +0100

    Added data_dirs setting to example

--- example.php
+++ example.php
@@ -11,6 +11,10 @@ $deploy = new Deploy(array(
 	'remote_dir' => '/home/user/project', // this is the same for all remote hosts if you define multiple
 	'remote_user' => 'user', // setup public key access to make it easy for yourself, many connections are made
 	'rsync_excludes' => 'config/rsync_exclude.txt',
+	'data_dirs' => array( // these dirs are stored separate from the other code and replaced by symlinks
+	    'web/uploads',
+	    'logs'
+	),
 	'target_specific_files' => array( // list of files that will be renamed on the remote host
 		'web/.htaccess',
 		'config/database.php'
