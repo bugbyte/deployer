015b64619ede075cf5f4914aa32a1678f46058ce
Bert-Jan de Lange <bert-jan@bugbyte.nl>
2013-03-07 16:38:17 +0100

    Change examples to put deploy_version one above the project root dir

--- apc/frontcontroller_example.php
+++ apc/frontcontroller_example.php
@@ -1,7 +1,7 @@
 <?php
 
-// first include the file containing the deployment timestamp (by default it's in the project root)
-require __DIR__ .'/../deploy_version.php';
+// first include the file containing the deployment timestamp (by default it's one directory above in the project root)
+require __DIR__ .'/../../deploy_version.php';
 
 // then include the code that check the timestamp and performs cache clearing operations if needed
 require __DIR__ .'/vendor/bugbyte/deployer/apc/handler.php';
